90b526db1db3ffecb8f0cd2645dfbe9f6b092d1f
Ru Chern Chong <hello@ruchern.dev>
2025-07-05 20:07:13 +0800

    fix: semantic release error

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   branches: ["main"],
   plugins: [
     "@semantic-release/commit-analyzer",
@@ -14,7 +14,8 @@ export default {
       "@semantic-release/git",
       {
         assets: ["CHANGELOG.md", "package.json"],
-        message: `chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}`,
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
     "@semantic-release/github",
